e0ab9e21af86d8eb491d82a97ce4938090a78730
C-NASIR <cnasir22.0@gmail.com>
2022-05-19 12:25:37 -0500

    Finished the styling of the Header and Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,18 @@
+import styles from "./Footer.module.css";
+
 function Footer() {
-  return "hello from footer";
+  return (
+    <div className={styles.footer}>
+      <div className={styles.logo}>
+        <p> Terms & Conditions </p>
+        <p> Privacy Policy </p>
+        <p> Cookie Policy </p>
+      </div>
+      <div className={styles.login}>
+        <p> Rebrandly @ Copyright 2020</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,18 @@
+import styles from "./Header.module.css";
+
 function Header() {
-  return "Hello from header ";
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <p> Headway </p>
+        <p> features </p>
+        <p> pricing </p>
+      </div>
+      <div className={styles.login}>
+        <p> Login</p>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
